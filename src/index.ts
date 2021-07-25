c179a59e4bee4cd672ae9166b4e5ad5e27e28d67
Your Name <you@example.com>
2021-07-25 22:59:09 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+class CompareCities {
+
+    constructor(cities: string[]) {
+
+        console.log("weather of " + cities);
+    }
+
+}
+
+function Test() {
+    const c1 = new CompareCities(["jerusalem2"])
+}
+
+Test();
