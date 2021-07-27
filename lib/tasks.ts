b8b9f7b5dfe9e7805925d7ef4c596a58a676d82a
ofer shaham <ofer.shaham@gmail.com>
2021-07-28 01:20:00 +0300

    test are passing apart to the integration test

---
+++ lib/tasks.ts
@@ -0,0 +1,69 @@
+import { GlobalAccumulator } from "./globalAccumulator";
+//stages: 
+//1 - get_minmax_temp_for_city:store data per city - for each day - save min temperature and max temperature
+//2 - compare all cities' data stored at stage 1 and find the min/max temperature for each day - and tag the temperature's city (it's source).
+//3 - get_rain_info - returns: is forcast indicates rain
+
+import Utils from "./utils";
+
+export const get_rain_info = (list): boolean => {
+    const res = list.filter((item: any) => { return Utils.isObjectWithData(item["rain"]); })
+    const rain_data = Utils.isObjectWithData(res);
+    return rain_data;
+}
+
+export const update_global_minMax = (output_cities_res: any) => {
+    const globalAccumulator = new GlobalAccumulator();
+    let acc_output_cities_res: any = {};
+    Object.keys(output_cities_res).forEach((city_name: string) => {
+        Object.keys(output_cities_res[city_name]).forEach((day: string) => {
+            const min_max_obj = output_cities_res[city_name][day];
+            globalAccumulator.update_day(day, city_name, min_max_obj);
+        });
+    });
+    return globalAccumulator;
+}
+
+
+export const get_minmax_temp_for_city = (list: any, city_name: string) => {
+
+    console.log({ city_name });
+
+    console.log(JSON.stringify(list, null, 4));
+
+    const item_ops = {
+        get_item_day: (item) => {
+            return item["dt_txt"].split(' ')[0];
+        },
+        get_item_min: (item) => {
+            return item["main"]["min_temp"];
+        },
+        get_item_max: (item) => {
+            return item["main"]["max_temp"];
+        }
+    }
+
+    function minMax(items) {
+        return items.reduce((acc, item) => {
+
+            const val_min: number = item_ops.get_item_min(item);
+            const val_max: number = item_ops.get_item_max(item);
+            const day_of_year: string = item_ops.get_item_day(item) as any;
+            const ref = acc[day_of_year];
+
+            //initial min,max entry for day
+            if (!ref) {
+                acc[day_of_year] = {
+                    min: val_min,
+                    max: val_max
+                };
+            } else {
+                ref["min"] = (ref["min"] === undefined || val_min < ref["min"]) ? val_min : ref["min"]
+                ref["max"] = (ref["max"] === undefined || val_max > ref["max"]) ? val_max : ref["max"]
+            }
+
+            return acc;
+        }, {});
+    }
+    return minMax(list);
+}
--- src/compare_cities.ts
+++ src/compare_cities.ts
@@ -10,51 +10,75 @@ import Utils from '../lib/utils';
 const config = require('../config/config.json');
 type CityTemperature = { city: string, temp: number }
 type TemperatureMinMax = { min: CityTemperature, max: CityTemperature }
-const fixtures_dir = path.resolve(`${__dirname}/../test/fixtures`);
 type Accumulator = Map<string, TemperatureMinMax>;
+import { get_minmax_temp_for_city, get_rain_info, update_global_minMax } from '../lib/tasks';
 
-class CompareCities {
+export class CompareCities {
+    private cities: string[];
+    path_storage: string;
 
-    constructor(cities: string[]) {
-        let five_days_accumulator = new Map();
-        console.log("weather of " + cities);
-        cities.forEach(async (city_name) => {
-            console.log("weather of " + city_name);
-
-            let data = await this.load_data(city_name).catch((err: any) => { throw err; });
-            data = JSON.parse(data);
-
-            // const max_temp = this.get_max_temp(city_name, data, five_days_accumulator);
-            // console.log(
-            //     city_name, max_temp
-            // );
-
-            const rain_data = this.get_rain(data);
-            console.log(
-                city_name, rain_data
-            );
-        })
-    }
-    get_max_temp(city_name: string, data: any, five_days_accumulator: Map<any, any>) {
-        throw new Error('Method not implemented.');
+    constructor(cities: string[], path_storage: string) {
+        this.path_storage = path_storage;
+        console.log("weather of cities: " + cities);
+        this.cities = cities;
     }
 
     load_data(city_name: string) {
-        return fsPromises.readFile(`${fixtures_dir}/${city_name}.json`, 'utf8');
+        const storage_dir = path.resolve(this.path_storage);
+
+        return fsPromises.readFile(`${storage_dir}/${city_name}.json`, 'utf8');
+    }
+    process_data_for_each_city(): Promise<any> {
+        let cityData; //load each json file in serial order, keeping memmory consumption low
+
+        let daily_min_max_temp_by_city = {};
+        let list_rain_info = {};
+
+        //resolve when is done
+        let counter = 0;
+        return new Promise(async (resolve, reject) => {
+            for (let city_name of this.cities) {
+                cityData = await this.load_data(city_name).catch(err => { return reject(err); });
+                if (!cityData) {
+                    return reject("empty file for city:" + city_name);
+                }
+                counter++;
+                console.log(counter + " of " + this.cities.length + " cities processed");
+
+                cityData = JSON.parse(cityData);
+
+                if (cityData.hasOwnProperty('list') && Array.isArray(cityData.list)) {
+                    daily_min_max_temp_by_city[city_name] = get_minmax_temp_for_city(cityData.list, city_name);
+                    list_rain_info[city_name] = get_rain_info(cityData.list);
+                } else {
+                    return reject("city.list data is not an array");
+                }
+                if (counter === this.cities.length) {
+                    console.log(JSON.stringify(daily_min_max_temp_by_city, null, 4));
+
+                    return resolve([daily_min_max_temp_by_city, list_rain_info]);
+                }
+            }
+        });
     }
 
-    get_rain(data: any) {
-        //todo: check if valid object
-        if (!Utils.isObjectWithData(data)) { throw new Error("invalid input"); }
-        const res = data.list.filter((item: any) => { return Utils.isObjectWithData(item.rain); })
-        return Utils.isObjectWithData(res);
+    async steps(): Promise<any> {
+        //
+        const res_process_data_for_each_city: any = await this.process_data_for_each_city().catch((err) => { console.log(err); process.exit(1); });
+        const [daily_min_max_temp_by_city, list_rain_info] = res_process_data_for_each_city as any;
+
+        const res_global_accumulator = update_global_minMax(daily_min_max_temp_by_city);
+        // console.log(daily_min_max_temp_by_city);
+        console.log(JSON.stringify(res_global_accumulator, null, 4));
+        console.log(list_rain_info);
+        return { res_global_accumulator, daily_min_max_temp_by_city, list_rain_info };
     }
-
-
 }
 
-function Test() {
-    const c1 = new CompareCities(config.cities_name);
+async function Test() {
+    const c1 = new CompareCities(config.cities_name, `${__dirname}/../storage`);
+    const res = await c1.steps();
+    console.log(res);
 }
 
 
